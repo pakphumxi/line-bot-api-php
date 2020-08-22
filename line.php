1d9816cb4fcb5bcf805024c108c2a2efc1c84d31
pakphumxi <70052235+pakphumxi@users.noreply.github.com>
2020-08-22 15:31:28 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '742Fl9coFh/IhuKPaFUhi5lPhlXA6nDg/jeLcHwS4S8PQ6dNx6m15sSXmmw4b6kBvj/fCI6uSB35oYoDHxh4cd625YnjK6eT2tSoO67MiUx3y/lNmxjP49cHjjEax0HJRw3tO5sUkSeC9comlSkcBAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
